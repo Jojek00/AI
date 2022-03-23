58bc45fa1a019d4181705435906faee94f8d6d9b
Jojek <olchowik0@gmail.com>
2022-03-23 11:44:45 +0100

    AI learning

---
+++ myperceptron.js
@@ -0,0 +1,26 @@
+function Perceptron(no, learningRate = 0.00001){
+    this.learnc = learningRate;
+    this.bias = 1;
+
+    this.weights = [];
+    for(let i = 0; i <= no; i++){
+        this.weights[i] = Math.random() * 2 - 1;
+    }
+    this.activate = function(inputs){
+        let sum = 0;
+        for(let i = 0; i < inputs.length; i++){
+            sum += inputs[i] * this.weights[i];
+        }
+        if(sum > 0) return 1; else return 0;
+    }
+    this.train = function(inputs, desired){
+        inputs.push(this.bias);
+        let guess = this.activate(inputs);
+        let error = desired - guess;
+        if(error != 0){
+            for(let i = 0; i < inputs.length; i++){
+                this.weights[i] += this.learnc * error * inputs[i];
+            }
+        }
+    }
+}
